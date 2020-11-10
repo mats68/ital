324925c710109a9d91e6d7645e66311b506610c3
Matthias Thaler <matthias.thaler@brunnerinformatik.ch>
2020-11-10 12:22:12 +0100

    div

--- build.ts
+++ build.ts
@@ -4,12 +4,12 @@ var fs = require('fs')
 const lineReader = require('line-reader');
 
 var input_file = 'MppPV20.cshtml'
-var output_file = 'MppPV20.after.cshtml'
-
-var arr_de = []
-var arr_it = []
 var input_de = 'trans/MppPV20.de.txt'
 var input_it = 'trans/MppPV20.it.txt'
+
+var output_str = ''
+var arr_de = []
+var arr_it = []
 
 lineReader.eachLine(input_de, function (line: string, last: boolean) {
     arr_de.push(line)
@@ -18,7 +18,7 @@ lineReader.eachLine(input_de, function (line: string, last: boolean) {
 lineReader.eachLine(input_it, function (line: string, last: boolean) {
     arr_it.push(line)
 })
-  
+
 if (arr_de.length !== arr_it.length) {
     console.error('Not same size!')
     process.exit()
@@ -26,23 +26,29 @@ if (arr_de.length !== arr_it.length) {
 
 lineReader.eachLine(input_file, function (line: string, last: boolean) {
     const ind = line.indexOf('GetText(')
+    let newLine = line
     if (ind > -1) {
         const ind1 = line.indexOf('"', ind + 1) + 1
         const ind2 = line.indexOf('"', ind1)
         const german = line.substring(ind1, ind2)
         const find = arr_de.findIndex(d => d === german)
         if (find > -1) {
-            // console.log(arr_de[find], '  ',arr_it[find] )
+            const ind3 = line.indexOf('"', ind2 + 1) + 1
+            const ind4 = line.indexOf('"', ind3 + 1) + 1
+            const str_before = line.substring(0, ind4)
+            const str_after = line.substring(ind4)
+            newLine = str_before + ',"' + arr_it[find] + '"' + str_after 
+            // console.log(newLine)
         }
-        const ind3 = line.indexOf('"', ind2 + 1) + 1
-        const ind4 = line.indexOf('"', ind3 + 1) + 1
-        const str_before = line.substring(0, ind4)
-        const str_after = line.substring(ind4)
-        const newLine = str_before + ',"' + arr_it[find] + '"' + str_after
-        console.log(newLine)
-
-
-        //console.log(german)
+    }
+    output_str = output_str + newLine + '\n' 
+    if (last) {
+        fs.writeFile('output/' + input_file, output_str, () => { })
     }
 })
+
+
+
+
+
 
